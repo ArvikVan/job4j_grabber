2ee1d67a3533aba83c101fe627bf885c1a4512d6
ArvikVan <arvikvan@gmail.com>
2021-09-05 01:12:19 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -2,7 +2,8 @@ package html;
 /**
  * класс описывает извлечение текста из html  по атрибутам
  * @author arvikv
- * @version 1.0
+ * @version 1.1
+ * 1.1 парсим первые 5 страниц
  */
 
 import org.jsoup.Jsoup;
@@ -12,15 +13,17 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get(); //получаем document
-        Elements row = doc.select(".postslisttopic"); // выбираем в document элементы с необходимым id
-        for (Element td : row) { // бежим по этим элементам
-            Element href = td.child(0); // и спускаемся вниз по дереву (child) и берем 0 элемент (в нашем случае их два)
-            System.out.println(href.attr("href")); //ссылка
-            System.out.println(href.text()); // текст ссылки
-            Element date = td.parent().child(5); // поднимаемся вверх по дереву и берем пятый элемент
-            // (он отвечает за время) т.к. altCol повторяется
-            System.out.println(date.text()); //получаем текст
+        for (int i = 0; i < 6; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get(); //получаем document
+            Elements row = doc.select(".postslisttopic"); // выбираем в document элементы с необходимым id
+            for (Element td : row) { // бежим по этим элементам
+                Element href = td.child(0); // и спускаемся вниз по дереву (child) и берем 0 элемент (в нашем случае их два)
+                System.out.println(href.attr("href")); //ссылка
+                System.out.println(href.text()); // текст ссылки
+                Element date = td.parent().child(5); // поднимаемся вверх по дереву и берем пятый элемент
+                // (он отвечает за время) т.к. altCol повторяется
+                System.out.println(date.text()); //получаем текст
+            }
         }
     }
 }
