b7b786f7352f14b2fa418a2b8dbee7393b7211fd
ArvikVan <arvikvan@gmail.com>
2021-09-05 00:47:58 +0300

    2.1. Преобразование даты [#289476]

--- src/test/java/utils/SqlRuDateTimeParserTest.java
+++ src/test/java/utils/SqlRuDateTimeParserTest.java
@@ -12,22 +12,22 @@ public class SqlRuDateTimeParserTest {
     public void data() {
         LocalDateTime localDateTime = new SqlRuDateTimeParser().parse("31 мая 00, 04:01");
         LocalDateTime res = LocalDateTime.of(
-                LocalDate.of(2000, 05, 31),
-                LocalTime.of(04,01));
+                LocalDate.of(2000, 5, 31),
+                LocalTime.of(4, 1));
         assertEquals(res, localDateTime);
     }
 
     @Test
     public void yesterday() {
         LocalDateTime yday = new SqlRuDateTimeParser().parse("вчера, 04:01");
-        LocalDateTime res = LocalDateTime.of(LocalDate.now().minusDays(1), LocalTime.of(04, 01));
+        LocalDateTime res = LocalDateTime.of(LocalDate.now().minusDays(1), LocalTime.of(4, 1));
         assertEquals(res, yday);
     }
 
     @Test
     public void today() {
         LocalDateTime today = new SqlRuDateTimeParser().parse("сегодня, 04:01");
-        LocalDateTime res = LocalDateTime.of(LocalDate.now(), LocalTime.of(04, 01));
+        LocalDateTime res = LocalDateTime.of(LocalDate.now(), LocalTime.of(4, 1));
         assertEquals(res, today);
     }
 
