d9d698020d52177fa9b521fd7824990c3b789413
ArvikVan <arvikvan@gmail.com>
2021-09-23 15:12:16 +0300

    0. Понятие сборки мусора [#6851]

---
+++ src/main/java/gc/GCDemo.java
@@ -0,0 +1,31 @@
+package gc;
+/**
+ *
+ * @author arvikv
+ * @version 1.0
+ */
+public class GCDemo {
+    private static final long KB = 1000;
+    private static final long MB = KB * KB;
+    private static final Runtime ENVIRONMENT = Runtime.getRuntime();
+    
+    public static void info() {
+        final long freeMemory = ENVIRONMENT.freeMemory();
+        final long totalMemory = ENVIRONMENT.totalMemory();
+        final long maxMemory = ENVIRONMENT.maxMemory();
+        System.out.println("==== Environment state ==== ");
+        System.out.printf("Free: %d%n", freeMemory / MB);
+        System.out.printf("Total: %d%n", totalMemory / MB);
+        System.out.printf("Max: %d%n", maxMemory / MB);
+    }
+    
+    public static void main(String[] args) {
+        info();
+        for (int i = 0; i < 30; i++) {
+            new Person(i, "N" + 1);
+        }
+        System.gc();
+        info();
+    }
+    
+}
