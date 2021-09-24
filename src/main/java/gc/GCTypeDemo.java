3a4e814889e62bae5d3e23c1bc22191c85c59c4e
ArvikVan <arvikvan@gmail.com>
2021-09-25 01:39:24 +0300

    0. Виды сборщиков мусора [#6852]

---
+++ src/main/java/gc/GCTypeDemo.java
@@ -0,0 +1,28 @@
+package gc;
+/**
+ * класс показывает работу с разными сборщиками
+ * @author arvikv
+ * @version 1.0
+ * Edit Configuration:
+ * - Serial => -XX:+UseSerialGC
+ * - Parallel => -XX:+UseParallelGC
+ * - CMS => -XX:+UseConcMarkSweepGC (допуступен до JDK 14)
+ * - G1 => -XX:+UseG1GC
+ * - ZGC => -XX:+UseZGC (use together, because error) -XX:+UnlockExperimentalVMOptions
+ * logs: -Xlog:gc* or -XX:-PrintGCDetails if jdk < 9
+ */
+
+import java.util.Random;
+
+public class GCTypeDemo {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int length = 100;
+        String[] data = new String[1_000_000];
+        for (int i = 0; ; i = (i + 1) % data.length) {
+            data[i] = String.valueOf(
+                    (char) random.nextInt(255)
+            ).repeat(length);
+        }
+    }
+}
