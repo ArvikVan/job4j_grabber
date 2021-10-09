c67014a27955c42ccb12ba11181384f67225839a
ArvikVan <arvikvan@gmail.com>
2021-10-09 23:50:10 +0300

    2. Что такое TDD? [#4918 #214192]fix(expected = AssertionError.class)

--- src/test/java/tdd/CinemaTest.java
+++ src/test/java/tdd/CinemaTest.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -16,7 +16,7 @@ import static org.junit.Assert.*;
  */
 public class CinemaTest {
 
-    @Test
+    @Test(expected = AssertionError.class)
     public void buy() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
@@ -26,7 +26,7 @@ public class CinemaTest {
         assertThat(ticket, is(new Ticket3D()));
     }
 
-    @Test
+    @Test (expected = AssertionError.class)
     public void find() {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
@@ -34,13 +34,12 @@ public class CinemaTest {
         assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
 
-    @Test
+    @Test (expected = AssertionError.class)
     public void add() {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
-        cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
-        assertThat(sessions, is(Arrays.asList(new Session3D(), new Session3D())));
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
 
 }
