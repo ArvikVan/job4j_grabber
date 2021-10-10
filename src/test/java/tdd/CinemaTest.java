b5112eb813c9d4b0d8aad9f94aa6f776773bf999
ArvikVan <arvikvan@gmail.com>
2021-10-10 21:31:35 +0300

    2. Что такое TDD? [#4918 #214192]

--- src/test/java/tdd/CinemaTest.java
+++ src/test/java/tdd/CinemaTest.java
@@ -1,5 +1,6 @@
 package tdd;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -11,11 +12,14 @@ import static org.junit.Assert.*;
 
 /**
  * @author arvikv
- * @version 1.0
+ * @version 1.1
  * @since 09.10.2021
+ * 1.1 Добавьте еще тесты на исключительные ситуации, когда на пример есть попытка купить билет
+ * на одно и то же место, купить на некорректное место или дату и т.п.
  */
 public class CinemaTest {
 
+    @Ignore
     @Test(expected = AssertionError.class)
     public void buy() {
         Account account = new AccountCinema();
@@ -26,6 +30,30 @@ public class CinemaTest {
         assertThat(ticket, is(new Ticket3D()));
     }
 
+    @Ignore
+    @Test(expected = AssertionError.class)
+    public void buy2Ticket1Place() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+        assertNull(ticket2);
+    }
+
+    @Ignore
+    @Test(expected = AssertionError.class)
+    public void wrongDate() {
+        Calendar premiereDay = Calendar.getInstance();
+        premiereDay.set(2021, 10, 10, 23, 00);
+        Calendar ticketDay = Calendar.getInstance();
+        ticketDay.set(2020, 10, 10, 23, 00);
+        assertNotEquals(premiereDay, ticketDay);
+    }
+
+    @Ignore
     @Test (expected = AssertionError.class)
     public void find() {
         Cinema cinema = new Cinema3D();
@@ -34,6 +62,7 @@ public class CinemaTest {
         assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
 
+    @Ignore
     @Test (expected = AssertionError.class)
     public void add() {
         Cinema cinema = new Cinema3D();
